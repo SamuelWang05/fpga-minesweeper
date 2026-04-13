4215f61ba2964c965d437b4864535737e3d74067
TheSemiColin <146498846+TheSemiColin@users.noreply.github.com>
2026-04-13 14:08:26 -0500

    Cursor Movement using PS/2 Driver

--- game_fsm.vhd
+++ game_fsm.vhd
@@ -16,11 +16,31 @@ entity GAME_FSM is
 end GAME_FSM;
 
 architecture skeleton of GAME_FSM is
+    signal row, col : unsigned(3 downto 0) := (others => '0');
 begin
+
     --cell_state <= (others => 'A'); -- All cells hidden ("00")
 	 cell_state <= x"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"; -- All cells revealed ("10")
-    cursor_row <= "0001";
-    cursor_col <= "0001";
+    cursor_row <= row;
+    cursor_col <= col;
     game_over  <= '0';
     game_won   <= '0';
+
+    cursor_proc : process(clk)
+    begin
+    if rising_edge(clk) then
+        if reset = '1' then
+            row <= "0000";
+            col <= "0000";
+        elsif move_up = '1' then
+            if row > 0 then row <= row - 1; end if;
+        elsif move_dn = '1' then
+            if row < 9 then row <= row + 1; end if;
+        elsif move_lt = '1' then
+            if col > 0 then col <= col - 1; end if;
+        elsif move_rt = '1' then
+            if col < 9 then col <= col + 1; end if;
+        end if;
+    end if;
+end process;
 end skeleton;
